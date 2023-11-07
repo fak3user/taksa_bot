66b0e8f26228ad0285698fdc8b20498414a8aa9b
fak3mistak3 <mirolyubov.dev@gmail.com>
2023-11-07 15:57:55 +0300

    add envs, update readme

---
+++ utils/envs.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Envs struct {
+	BotToken string
+	GptToken string
+	DbUri    string
+}
+
+func GetEnvs() (Envs, error) {
+	godotenv.Load()
+	var envs Envs
+
+	envs.BotToken = os.Getenv("BOT_TOKEN")
+	envs.DbUri = os.Getenv("DB_URI")
+
+	return envs, nil
+}
