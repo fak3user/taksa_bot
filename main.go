ec10ef8d75a0bbd5a562677e90dd965e54fcc03e
fak3mistak3 <mirolyubov.dev@gmail.com>
2023-11-07 17:08:13 +0300

    add echo command to bot

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"taksa/bot"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
 
 func main() {
+	err := <-bot.Init()
+	if err != nil {
+		panic(err)
+	}
+
+	tgbot := bot.GetBot()
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := tgbot.GetUpdatesChan(u)
+
+	for update := range updates {
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		echoMsg := tgbotapi.NewMessage(update.Message.From.ID, update.Message.Text)
+		tgbot.Send(echoMsg)
+	}
 	fmt.Println("321")
 }
