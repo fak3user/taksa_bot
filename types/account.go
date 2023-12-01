a0139ea76d784728a5c6bc774c47fbab48e30dab
fak3mistak3 <mirolyubov.dev@gmail.com>
2023-12-01 20:45:37 +0400

    merge with refactor

--- types/account.go
+++ types/account.go
@@ -8,5 +8,5 @@ type Account struct {
 	Description  string               `bson:"description"`
 	Opened       bool                 `bson:"opened"`
 	Transactions []primitive.ObjectID `bson:"transactions"`
-	Participantsvfdkopsgkkfg []primitive.ObjectID `bson:"participants"`
+	Participants []primitive.ObjectID `bson:"participants"`
 }
