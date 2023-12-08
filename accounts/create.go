1e9d623ae5dfbf2282ca3026b94519631d004b33
fak3mistak3 <mirolyubov.dev@gmail.com>
2023-12-08 19:57:39 +0400

    wip: creating account

--- accounts/create.go
+++ accounts/create.go
@@ -1,5 +1,25 @@
 package accounts
 
-func CreateAccount() {
+import (
+	"taksa/types"
+)
+
+// import db and handlers
+
+func InitAccount(userId int64) (types.Account, error) {
+	var account types.Account
+
+	return account, nil
+}
+
+func UpdateAccount(account types.Account) (types.Account, error) {
+	return account, nil
+}
+
+func CloseAccount() {
+
+}
+
+func DeleteAccount() {
 
 }
--- handlers/update_handler.go
+++ handlers/update_handler.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"log"
+	"taksa/accounts"
 	"taksa/bot"
 	"taksa/texts"
 
@@ -9,66 +10,67 @@ import (
 )
 
 func HandleUpdate(updates tgbotapi.UpdatesChannel, botInstance *tgbotapi.BotAPI) {
-    for update := range updates {
-        if update.Message != nil {
-            logMessage(update)
-            handleMessage(update, botInstance)
-        }		
-    }
+	for update := range updates {
+		if update.Message != nil {
+			logMessage(update)
+			handleMessage(update, botInstance)
+		}
+	}
 }
 
 // logMessage logs the incoming message
 func logMessage(update tgbotapi.Update) {
-    log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+	log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 }
 
 // handleMessage processes the received message based on its type and content
 func handleMessage(update tgbotapi.Update, botInstance *tgbotapi.BotAPI) {
-    var msg tgbotapi.MessageConfig
+	var msg tgbotapi.MessageConfig
 
-    if update.Message.Chat.Type == "group" {
-        msg = handleGroupMessage(update, botInstance)
-    } else {
-        msg = handlePrivateMessage(update, botInstance)
-    }
+	if update.Message.Chat.Type == "group" {
+		msg = handleGroupMessage(update, botInstance)
+	} else {
+		msg = handlePrivateMessage(update, botInstance)
+	}
 
-    if msg.Text != "" {
-        msg.ReplyToMessageID = update.Message.MessageID
-        botInstance.Send(msg)
-    }
+	if msg.Text != "" {
+		msg.ReplyToMessageID = update.Message.MessageID
+		botInstance.Send(msg)
+	}
 }
 
 // handleGroupMessage processes messages from group chats
 func handleGroupMessage(update tgbotapi.Update, botInstance *tgbotapi.BotAPI) tgbotapi.MessageConfig {
-    if update.Message.IsCommand() {
-        return processGroupCommand(update)
-    } else if isNewMember(update, botInstance) {
-        return processNewGroupMember(update)
-    }
-    return tgbotapi.NewMessage(update.Message.Chat.ID, "test") // default response
+	if update.Message.IsCommand() {
+		return handleGroupCommand(update)
+	} else if isNewMember(update, botInstance) {
+		return handleNewGroupMember(update)
+	}
+	return tgbotapi.NewMessage(update.Message.Chat.ID, "test") // default response
 }
 
 // handlePrivateMessage processes messages from private chats
 func handlePrivateMessage(update tgbotapi.Update, botInstance *tgbotapi.BotAPI) tgbotapi.MessageConfig {
-    if update.Message.IsCommand() {
-        return processPrivateCommand(update, botInstance)
-    }
-    return tgbotapi.NewMessage(update.Message.From.ID, "test") // default response
+	if update.Message.IsCommand() {
+		return handlePrivateCommand(update, botInstance)
+	}
+	return tgbotapi.NewMessage(update.Message.From.ID, "test") // default response
 }
 
-func processGroupCommand(update tgbotapi.Update) tgbotapi.MessageConfig {
-    var msg tgbotapi.MessageConfig
-    switch update.Message.Command() {
-    case "new_order":
-        msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Catch!")
-        // Add more cases as necessary
-    }
-    return msg
+func handleGroupCommand(update tgbotapi.Update) tgbotapi.MessageConfig {
+	var msg tgbotapi.MessageConfig
+	switch update.Message.Command() {
+	case "new_order":
+		account, err := accounts.CreateAccount()
+		msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Catch!")
+		// Add more cases as necessary
+	}
+	return msg
 }
 
-func processNewGroupMember(update tgbotapi.Update) tgbotapi.MessageConfig {
-    var msg tgbotapi.MessageConfig
-    ok, err := bot.AddToChat(update.Message.Chat)
+func handleNewGroupMember(update tgbotapi.Update) tgbotapi.MessageConfig {
+	var msg tgbotapi.MessageConfig
+	ok, err := bot.AddToChat(update.Message.Chat)
 	if err != nil {
 		panic(err)
 	}
@@ -77,22 +79,21 @@ func processNewGroupMember(update tgbotapi.Update) tgbotapi.MessageConfig {
 	} else {
 		msg = tgbotapi.NewMessage(update.Message.Chat.ID, texts.MessageAlreadyAddedToGroup)
 	}
-    return msg
+	return msg
 }
 
-
-func processPrivateCommand(update tgbotapi.Update, botInstance *tgbotapi.BotAPI) tgbotapi.MessageConfig {
-    var msg tgbotapi.MessageConfig
-    switch update.Message.Command() {
-    case "start":
-        // Implement logic for start command
-        msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Welcome!")
-        // Add more cases as necessary
-    }
-    return msg
+func handlePrivateCommand(update tgbotapi.Update, botInstance *tgbotapi.BotAPI) tgbotapi.MessageConfig {
+	var msg tgbotapi.MessageConfig
+	switch update.Message.Command() {
+	case "start":
+		// Implement logic for start command
+		msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Welcome!")
+		// Add more cases as necessary
+	}
+	return msg
 }
 
 func isNewMember(update tgbotapi.Update, botInstance *tgbotapi.BotAPI) bool {
-    // Assuming the first user in NewChatMembers is the one who joined
-    return update.Message.NewChatMembers != nil && len(update.Message.NewChatMembers) > 0 && update.Message.NewChatMembers[0].ID == botInstance.Self.ID
+	// Assuming the first user in NewChatMembers is the one who joined
+	return update.Message.NewChatMembers != nil && len(update.Message.NewChatMembers) > 0 && update.Message.NewChatMembers[0].ID == botInstance.Self.ID
 }
